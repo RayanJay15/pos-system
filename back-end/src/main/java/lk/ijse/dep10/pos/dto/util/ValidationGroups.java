e47b80507e58629ac34b7938015f2db2dc67b8f0
rajitha jayarathne <rajithajayarathne94@gmail.com>
2023-06-24 01:27:54 +0530

    OrderDTO2 added and validation group created

---
+++ back-end/src/main/java/lk/ijse/dep10/pos/dto/util/ValidationGroups.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep10.pos.dto.util;
+
+import javax.validation.groups.Default;
+
+public interface ValidationGroups {
+
+    interface Update extends Default{
+
+    }
+
+    interface Save extends Default{
+
+    }
+
+    interface Delete extends Default{
+
+    }
+
+}
